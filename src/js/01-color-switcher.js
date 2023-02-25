ab348ac7108fae78e0dd6b0e5883271cd6ee4158
Yuriy-risk <111949781+Yuriy-risk@users.noreply.github.com>
2023-02-25 18:40:39 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const bodyEl = document.querySelector('body');
+const startButtonEl = document.querySelector('button[data-start]');
+const stopButtonEl = document.querySelector('button[data-stop]');
+stopButtonEl.setAttribute('disabled', '');
+let intID;
 
+startButtonEl.addEventListener('click', () => {
+  buttonToggler();
+  colorChanger();
+});
+
+stopButtonEl.addEventListener('click', () => {
+  buttonToggler();
+  clearInterval(intID);
+});
+
+function buttonToggler() {
+  startButtonEl.toggleAttribute('disabled');
+  stopButtonEl.toggleAttribute('disabled');
+}
+
+function colorChanger() {
+  intID = setInterval(
+    () => (bodyEl.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
